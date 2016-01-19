5b40772072c5860c40364fd725c67e575930dadb
Mike Land <mikepland@gmail.com>
2016-01-18 21:35:16 -0600

    sets up func

--- artofwarEXAMPLE.sh
+++ artofwarEXAMPLE.sh
@@ -1,3 +1,7 @@
 a1="12. When their substance is drained away, the peasantry will be afflicted by heavy exactions."
 
 a2="13,14. With this loss of substance and exhaustion of strength, the homes of the people will be stripped bare, and three-tenths of their income will be dissipated; while government expenses for broken chariots, worn-out horses, breast-plates and helmets, bows and arrows, spears and shields, protective mantles, draught-oxen and heavy wagons, will amount to four-tenths of its total revenue."
+
+a3="12. When their substance is drained away, the peasantry will be afflicted by heavy exactions. 12. When their substance is drained away, the peasantry will be afflicted by heavy exactions."
+
+a4="1.  Sun Tzu said: In the operations of war, where there are in the field a thousand swift chariots, as many heavy chariots, and a hundred thousand mail-clad soldiers, with provisions enough to carry them a thousand LI, the expenditure at home and at the front, including entertainment of guests, small items such as glue and paint, and sums spent on chariots and armor, will reach the total of a thousand ounces of silver per day. Such is the cost of raising an army of 100,000 men."
--- scriptActual.sh
+++ scriptActual.sh
@@ -1,30 +1,21 @@
 . ./artofwarEXAMPLE.sh
 
-declare -a arr=("$a1" "$a2")
+declare -a arr=("$a1" "$a2" "$a3" "$a4")
 
 x=0
 for i in "${arr[@]}"
 do
-    if [ $x -ge 15 ]; then
-        echo "over"; exit
-    else
-        echo "under"
-    fi 
+   x=$((x+1))
+   title="artOfWar"${x}".png"
+   echo "creating $title"
 
-
-
-   # x=$((x+1))
-   # title="artOfWar"${x}".png"
-   # convert -background white -fill black \
-   #     -font pala.ttf -pointsize 24 -size 400x225 \
-   #     -gravity center \
-   #     caption:"$i" \
-   #     -fill grey \
-   #     -pointsize 16 \
-   #     -gravity North -annotate +0+8 'The Art of War - Sun Tzu' \
-   #     $title
-
-
+    convert -background white -fill black \
+        -font pala.ttf -size 400x225 \
+        -gravity north \
+        caption:"\n$i" \
+        -fill grey \
+        -pointsize 14 \
+        -annotate +0+2 'The Art of War - Sun Tzu' \
+        $title
 
 done
-
